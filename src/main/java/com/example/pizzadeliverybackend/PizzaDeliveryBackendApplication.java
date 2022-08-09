1c15d9db4c85b223b89cab16509db92f48e5fbca
Joao Renault <Joaomarcelo.Renault@emea.merkleinc.com>
2022-08-09 11:39:11 +0100

    initial commit

---
+++ src/main/java/com/example/pizzadeliverybackend/PizzaDeliveryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzadeliverybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaDeliveryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaDeliveryBackendApplication.class, args);
+    }
+
+}
