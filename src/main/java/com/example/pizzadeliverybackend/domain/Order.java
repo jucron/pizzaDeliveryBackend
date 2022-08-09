7c11605e80f44fded238b11564273dd3dccad298
Joao Renault <Joaomarcelo.Renault@emea.merkleinc.com>
2022-08-09 11:46:38 +0100

    Creates Order.java

---
+++ src/main/java/com/example/pizzadeliverybackend/domain/Order.java
@@ -0,0 +1,34 @@
+package com.example.pizzadeliverybackend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String clientName;
+    private String pizzaFlavor;
+    private String address;
+    private String status;
+    private LocalTime orderTime;
+    private boolean paid;
+
+
+}
